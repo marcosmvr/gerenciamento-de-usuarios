4133a0fe2d3616d6ff35f2ba7557a65a1c760a0d
marcosmvr <marcosviniciusreis260@gmail.com>
2025-04-08 17:34:29 -0300

    adicionei o prisma

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import helmet from 'helmet'
 import express from 'express'
-
 const app = express()
+import userRoutes from './routes/userRoutes.js'
+const PORT = process.env.PORT || 3000
 
 app.use(helmet())
+app.use(express.json())
+app.use('/', userRoutes)
 
-app.listen(3000, () => {
-  console.log('rodando na porta 3000')
+app.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}`)
 })
